23c27a852a0a3a328499be4dff25eb306c298058
bone0825 <quddlf775@naver.com>
2024-01-10 10:43:11 +0900

    first commit

---
+++ src/main/java/org/zerock/w1/HelloServlet.java
@@ -0,0 +1,32 @@
+package org.zerock.w1;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        System.out.println("안녕하세요");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
